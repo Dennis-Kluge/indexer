cfe51fc8ecdef1ddf9e6b73bc92eec4f45f5c4f1
Horst Mumpitz <dennis.kluge@gmx.de>
2012-05-20 16:35:09 +0200

    refactored indexer code moved to implementation

--- src/FileIndexer.cpp
+++ src/FileIndexer.cpp
@@ -2,12 +2,74 @@
 
 using namespace std ;
 
-FileIndexer::FileIndexer(string file)
+// FileIndexer::FileIndexer(string file)
+// {
+// 	mFile = file;
+// }
+
+
+// void FileIndexer::IndexFile(){
+// 	cout << nFile;
+// }
+
+FileIndexer::FileIndexer(char file[])
 {
 	mFile = file;
 }
 
+void FileIndexer::PrintResult() {
+	PrintResult(indexMap);	
+}
+	
 
-void FileIndexer::IndexFile(){
-	cout << nFile;
+void FileIndexer::PrintResult(MyMap mmap){
+	cout << "Ausgabe:\n";
+    MyMap::iterator iter = mmap.begin();
+    while(iter != mmap.end())
+    {     
+    	cout << (*iter).first << "(";
+    	std::vector<string>::iterator itvec;
+    	for(itvec = (*iter).second.begin(); itvec != (*iter).second.end(); itvec++) {
+    		cout << *itvec << "+";
+    	}
+    	cout << ")" <<endl;
+         ++iter;
+    }
+}
+
+map<string, vector<string> > FileIndexer::IndexFile(){
+	ifstream ifs(mFile) ;
+    if (!ifs)
+    {
+        cout << "Eingabedatei \"" << mFile << "\" konnte nicht geoeffnet werden!" << endl ;
+        exit(EXIT_FAILURE) ;
+    }
+    string line;
+    int i = 1;
+    while(getline(ifs, line)) {
+    	string buf;
+    	stringstream ss(line);
+    	int j = 1;
+    	while(ss >> buf) {
+    		vector<string> words;
+    		stringstream row;
+    		stringstream column;
+    		row << i;
+    		column << j;
+    		string position = "(" + row.str() + "," + column.str() + ")";
+    		MyMap::iterator it;
+    		it = indexMap.find(buf);
+    		if(it != indexMap.end()) {
+    			words = (*it).second;
+    			indexMap.erase(it);
+    		}
+    		words.push_back(position);
+    		indexMap.insert( make_pair(buf,words));
+    		j++;
+    	}
+    	i++;
+    }
+    ifs.close() ;
+    
+   return indexMap;
 }
--- src/FileIndexer.hpp
+++ src/FileIndexer.hpp
@@ -1,6 +1,3 @@
-#ifndef FILEINDEXER_HPP
-#define FILEINDEXER_HPP
-
 #include <cstdlib>
 #include <string>
 #include <map>
@@ -17,85 +14,19 @@ using namespace std ;
 class FileIndexer
 {
 
+    public:
+    	FileIndexer(char file[]);
+    	map<string, vector<string> > IndexFile();
+    	void PrintResult(map<string, vector<string> >  mmap);
+    	void PrintResult();
 
-
-public:
-	FileIndexer(char file[]);
-	map<string, vector<string> > IndexFile();
-	void PrintResult(map<string, vector<string> >  mmap);
-	void PrintResult();
-
-private:
-	fstream stream;
-	string content;
-	char *mFile;
-	typedef map<string, vector<string> > MyMap;
-	MyMap indexMap;
-	FileIndexer() {}
-
+    private:
+    	fstream stream;
+    	string content;
+    	char *mFile;
+    	typedef map<string, vector<string> > MyMap;
+    	MyMap indexMap;
+    	FileIndexer() {}
 
 } ;
 
-#endif
-
-FileIndexer::FileIndexer(char file[])
-{
-	mFile = file;
-}
-
-void FileIndexer::PrintResult() {
-	PrintResult(indexMap);	
-}
-	
-
-void FileIndexer::PrintResult(MyMap mmap){
-	cout << "Ausgabe:\n";
-    MyMap::iterator iter = mmap.begin();
-    while(iter != mmap.end())
-    {     
-    	cout << (*iter).first << "(";
-    	std::vector<string>::iterator itvec;
-    	for(itvec = (*iter).second.begin(); itvec != (*iter).second.end(); itvec++) {
-    		cout << *itvec << "+";
-    	}
-    	cout << ")" <<endl;
-         ++iter;
-    }
-}
-
-map<string, vector<string> > FileIndexer::IndexFile(){
-	ifstream ifs(mFile) ;
-    if (!ifs)
-    {
-        cout << "Eingabedatei \"" << mFile << "\" konnte nicht geoeffnet werden!" << endl ;
-        exit(EXIT_FAILURE) ;
-    }
-    string line;
-    int i = 1;
-    while(getline(ifs, line)) {
-    	string buf;
-    	stringstream ss(line);
-    	int j = 1;
-    	while(ss >> buf) {
-    		vector<string> words;
-    		stringstream row;
-    		stringstream column;
-    		row << i;
-    		column << j;
-    		string position = "(" + row.str() + "," + column.str() + ")";
-    		MyMap::iterator it;
-    		it = indexMap.find(buf);
-    		if(it != indexMap.end()) {
-    			words = (*it).second;
-    			indexMap.erase(it);
-    		}
-    		words.push_back(position);
-    		indexMap.insert( make_pair(buf,words));
-    		j++;
-    	}
-    	i++;
-    }
-    ifs.close() ;
-    
-   return indexMap;
-}
