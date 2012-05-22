bc682ee7b2497b90849588467e5886d69245be7c
Horst Mumpitz <dennis.kluge@gmx.de>
2012-05-22 09:58:43 +0200

    added test data

---
+++ Testdaten/Test.c
@@ -0,0 +1,9 @@
+Alan Turing and his Contemporaries: Building the World's First Computers
+Abstract
+To mark the centenary of the birth of Alan Turing, the British Computer Society 
+has published a book written by members of the Computer Conservation Society.
+The book examines Alan Turing's contribution to the implementation of practical 
+stored-program digital computers and the relation between Turing and the other 
+computer pioneers active in the period 1945–55. In this invited article,
+the book's Editor gives an overall view of the subject, focussing particularly
+on Alan Turing's special contributions in the first decade of the Information Age.
