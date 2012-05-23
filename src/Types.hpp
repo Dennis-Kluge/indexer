67d0cab3e45f61e1e4521fc46a758279981313a4
Horst Mumpitz <dennis.kluge@gmx.de>
2012-05-24 00:15:06 +0200

    refactored from Types.h to Types.hpp

---
+++ src/Types.hpp
@@ -0,0 +1,33 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <set>
+#include <map>
+#include <string>
+#include <vector>
+#include "LexComparator.hpp"
+
+using namespace std;
+
+
+/*!
+* Deklaration der Datenstruktur des Indexers.
+* @author Swen Hutop
+* @author Dennis Kluge
+*/
+
+/*!
+* Ein Entry besteht aus einem Pair von ints
+*/
+typedef pair<int, int> Entry;
+
+/*!
+* Der Index besteht aus eine Map deren Key das Wort und als Value 
+* ein vector von Entries genutzt wird. 
+* Die Sortierung übernimmt der LexComparator 
+*/
+typedef map<string, vector<Entry>, LexComparator> MyMap;
+
+#endif
+
+
