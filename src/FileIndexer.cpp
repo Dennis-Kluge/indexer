5ee5d33920cca1ab5188952967fbc27a7a13f72f
Horst Mumpitz <dennis.kluge@gmx.de>
2012-05-23 22:48:50 +0200

    refactored and documentation

--- src/FileIndexer.cpp
+++ src/FileIndexer.cpp
@@ -2,109 +2,77 @@
 
 using namespace std ;
 
-// FileIndexer::FileIndexer(string file)
-// {
-// 	mFile = file;
-// }
-
-
-// void FileIndexer::IndexFile(){
-// 	cout << nFile;
-// }
-
-FileIndexer::FileIndexer(string file)
-{
+FileIndexer::FileIndexer(string file) {
 	mFile = file;
 }
 
-void FileIndexer::PrintPositionVector(vector<Entry> positions) 
-{
-    vector<Entry>::iterator itvec;
-    for(itvec = positions.begin(); itvec != positions.end(); ++itvec) {
-        Entry pos = *itvec;
+void FileIndexer::PrintPositionVector(vector<Entry> positions)  {
+    vector<Entry>::iterator entry_iterator;
+    for(entry_iterator = positions.begin(); entry_iterator != positions.end(); ++entry_iterator) {
+        Entry pos = *entry_iterator;
         cout << " (" << pos.first << "," << pos.second << ")" ;
     }
     cout << endl;
 }
 
-void FileIndexer::PrintWordIndex(string word) 
-{
-    MyMap::iterator it = indexMap.find(word);
-     if(it != indexMap.end()) 
-     {
-        PrintPositionVector((*it).second);
-     } else 
-     {
+void FileIndexer::PrintWordIndex(string word) {
+    MyMap::iterator iterator = indexMap.find(word);
+    if(iterator != indexMap.end()) {
+        PrintPositionVector((*iterator).second);
+    } else {
         cout << "Das Wort ist nicht im Index vorhanden" ; 
-     }
-
-            
+    }            
 }
 
-void FileIndexer::PrintWordsIndexes(string word) 
-{
-
-    MyMap::iterator it ;
+void FileIndexer::PrintWordsIndexes(string word)  {
+    MyMap::iterator iterator;
     MyMap result; 
     if(word.size() == 0) {
-        PrintResult();
+        PrintResult(indexMap);
     } else {
-        for(it = indexMap.begin(); it != indexMap.end(); ++it) 
-        {
-            string sub = (*it).first.substr(0,word.size());
+        for(iterator = indexMap.begin(); iterator != indexMap.end(); ++iterator) {
+            string sub = (*iterator).first.substr(0,word.size());
             transform(word.begin(), word.end(), word.begin(), ::tolower);
             transform(sub.begin(), sub.end(), sub.begin(), ::tolower);
-           if(word == sub)
-            {
-                result.insert(make_pair((*it).first,(*it).second)) ;   
+           if(word == sub) {
+                result.insert(make_pair((*iterator).first,(*iterator).second)) ;   
             } 
         }
-        if(result.size() == 0)
-        {
+        if(result.size() == 0) {
             cout << "Es sind keine Wörter mit diesen Anfangsbuchstaben vorhanden" ;
-        } else 
-        {
+        } else {
             PrintResult(result) ;    
         }
-    }
-    
-}
+    }    
+}	
 
-void FileIndexer::PrintResult() {
-	PrintResult(indexMap);	
-}
-	
-
-void FileIndexer::PrintResult(MyMap mmap){
-    MyMap::iterator iter = mmap.begin();
-    while(iter != mmap.end())
-    {     
-    	cout << (*iter).first;
-    	PrintPositionVector(((*iter).second));
-         ++iter;
+void FileIndexer::PrintResult(MyMap mmap) {
+    MyMap::iterator iterator = mmap.begin();
+    while(iterator != mmap.end()) {     
+    	cout << (*iterator).first;
+    	PrintPositionVector(((*iterator).second));
+         ++iterator;
     }
 }
 
-MyMap FileIndexer::IndexFile(){
-	ifstream ifs(mFile.data()) ;
-    if (!ifs)
-    {
+MyMap FileIndexer::IndexFile() {
+	ifstream inputStream(mFile.data()) ;
+    if (!inputStream) {
         cout << "Eingabedatei \"" << mFile << "\" konnte nicht geoeffnet werden!" << endl ;
-        exit(EXIT_FAILURE) ;
+        exit(EXIT_FAILURE);
     }
     string line;
     int i = 1;
     Parser myParser;
-    while(getline(ifs, line)) {
-    	stringstream ss(line) ;
+    while(getline(inputStream, line)) {    	
     	int j = 1 ;
-        vector<string> lineWords = myParser.parse_input_line(line) ;
-        vector<string>::iterator lwi;
+        vector<string> lineWords = myParser.parseInputLine(line) ;
+        vector<string>::iterator lineWordsIterator;
         bool replace;
-        for(lwi = lineWords.begin(); lwi != lineWords.end(); ++lwi) {
+        for(lineWordsIterator = lineWords.begin(); lineWordsIterator != lineWords.end(); ++lineWordsIterator) {
             vector<Entry> words;
             Entry position (i,j);
-            string word = *lwi ;
+            string word = *lineWordsIterator ;
             words = indexMap[word];        
             words.push_back(position);
             indexMap[word] = words;
@@ -112,8 +80,7 @@ MyMap FileIndexer::IndexFile(){
         }
     	i++;
     }
-    ifs.close() ;
-    
+    inputStream.close() ;   
    return indexMap;
 }
 
